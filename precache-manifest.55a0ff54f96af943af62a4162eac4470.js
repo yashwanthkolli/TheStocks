3a205a7108e9722b6e7d2f859827e13340046bb1
unknown <yashwanthkolli3271gmail.com>
2020-06-21 09:57:45 +0530

    Updates

---
+++ precache-manifest.55a0ff54f96af943af62a4162eac4470.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf7795b91dd3e1ebcc69c949ee95ed3",
+    "url": "/TheStocks/index.html"
+  },
+  {
+    "revision": "166ecbc48fc4e04f028c",
+    "url": "/TheStocks/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b1dad5ca5246ecfa4a41",
+    "url": "/TheStocks/static/css/main.a7ac9d91.chunk.css"
+  },
+  {
+    "revision": "166ecbc48fc4e04f028c",
+    "url": "/TheStocks/static/js/2.eb31af32.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/TheStocks/static/js/2.eb31af32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1dad5ca5246ecfa4a41",
+    "url": "/TheStocks/static/js/main.27d191bf.chunk.js"
+  },
+  {
+    "revision": "2adfddbe346effd4ad13",
+    "url": "/TheStocks/static/js/runtime-main.4f362ff7.js"
+  }
+]);
